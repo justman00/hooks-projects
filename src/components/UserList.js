2b8e13f7a72bad124af62818513f1b8c60fd98ce
Vlad <vovaturcan4@gmail.com>
2019-02-16 20:09:20 +0200

    done my first project using hooks

---
+++ src/components/UserList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useResources } from "./useResources";
+
+export default function UserList() {
+  const users = useResources("users");
+  return (
+    <div>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
